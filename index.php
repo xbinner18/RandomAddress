1d07881734b20b02e93f6228144c487d3de9136f
Nitin1818 <74262832+xbinner18@users.noreply.github.com>
2021-12-15 10:48:53 +0530

    Update

--- index.php
+++ index.php
@@ -7,67 +7,17 @@ function GetStr($string, $start, $end)
   return $str[0];
 }
 
-function randomName() {
-    $names = array(
-        'Mischke',
-        'Serna',
-        'Pingree',
-        'Mcnaught',
-        'Pepper',
-        'Schildgen',
-        'Mongold',
-        'Wrona',
-        'Geddes',
-        'Lanz',
-        'Fetzer',
-        'Schroeder',
-        'Block',
-        'Mayoral',
-        'Fleishman',
-        'Roberie',
-        'Latson',
-        'Lupo',
-        'Motsinger',
-        'Drews',
-        'Coby',
-        'Redner',
-        'Culton',
-        'Howe',
-        'Stoval',
-        'Michaud',
-        'Mote',
-        'Menjivar',
-        'Wiers',
-        'Paris',
-        'Grisby',
-        'Noren',
-        'Damron',
-        'Kazmierczak',
-        'Haslett',
-        'Guillemette',
-        'Buresh',
-        'Center',
-        'Kucera',
-        'Catt',
-        'Badon',
-        'Grumbles',
-        'Antes',
-        'Byron',
-        'Volkman',
-        'Klemp',
-        'Pekar',
-        'Pecora',
-        'Schewe',
-        'Ramage',
-    );
-    return $names[rand ( 0 , count($names) -1)];
+function multiexplode($delimiters, $string)
+{
+  $one = str_replace($delimiters, $delimiters[0], $string);
+  $two = explode($delimiters[0], $one);
+  return $two;
 }
 
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, 'https://www.fakeaddressgenerator.com/');
+curl_setopt($ch, CURLOPT_URL, 'https://fakepersongenerator.com/fake-name-generator');
 curl_setopt($ch, CURLOPT_HEADER, 0);
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-'Host: www.fakeaddressgenerator.com',
 'User-Agent: Mozilla/5.0 (Linux; Android 10; vivo 1806) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.64 Mobile Safari/537.36',
 'Accept-Language: en-US,en;q=0.9',
 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'));
@@ -76,15 +26,16 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
 $res = curl_exec($ch);
-$name = trim(strip_tags(getStr($res,'<td><span>Full Name</span></td>','</b>')));
-$first = trim(strip_tags(getStr($res,'<td><b>','&nbsp;')));
-$last = randomName();
-$street = trim(strip_tags(getStr($res,'<td>Street</td>','</b></td>')));
-$city = trim(strip_tags(getStr($res,'<td>City</td>','</b></td>')));
-$state = trim(strip_tags(getStr($res,'State/Province abbr','</b></td>')));
-$statefull = trim(strip_tags(getStr($res,'State/Province full','</b></td>')));
-$zip = trim(strip_tags(getStr($res,'<td>Zip Code/Postal code','</b></td>')));
-$phone = trim(strip_tags(getStr($res,'Phone Number','</b></td>')));
+$name = trim(strip_tags(getStr($res,"class='text-center name'><b class='click'>",'</b>')));
+$first = multiexplode(array("&nbsp;"), $name)[0];
+$last = multiexplode(array("&nbsp;"), $name)[2];
+$street = trim(strip_tags(getStr($res,'<p>Street: <b>','</b>')));
+$stct = trim(strip_tags(getStr($res,'<p>City, State, Zip: <b>','</b>')));
+$city = multiexplode(array(","), $stct)[0];
+$statefull = multiexplode(array(","), $stct)[1];
+$state = trim(strip_tags(getStr($statefull,'(',')')));
+$zip = multiexplode(array(","), $stct)[2];
+$phone = trim(strip_tags(getStr($res,"title='test'>Mobile: <b>",'</b>')));
 
 if($res == null){
   echo '{"message":"failed to get address"}';
